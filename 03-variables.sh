d0953b3e9c855b07ac0f53ccb4dd85de91b7d100
saisuda9 <saikumarsuda@gmail.com>
2021-12-03 19:56:04 +0530

    add new line

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 ## name assigned to data is variable
+## i.e var=data
+
+## in shell no data types , shell consider everything as a string
 
 a=100
 b=abc
+
+## to access data use $ before the variable name
 
 echo value of a=$a
 echo value of b=$b
