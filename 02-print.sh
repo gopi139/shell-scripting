d1924e8d471ceecdcf0a8554f17583d87f7a5127
saisuda9 <saikumarsuda@gmail.com>
2021-12-03 17:53:00 +0530

    add colour codes

--- 02-print.sh
+++ 02-print.sh
@@ -7,6 +7,16 @@ echo new world
 echo -e "welcome\nnewworld\nhai"
 echo -e "word 1\t\t word2\t\tword 3"
 
+## 31 -- red colour
+## 32 -- green colour
+## 33 -- yellow colour
+## 34 -- blue colour
+## 35 -- magenta
+## 36 -- cyan
+
+
+
+
 echo -e "\e[31mtext in red colour"
 echo -e "\e[32mtext in green colour"
 echo -e "\e[33mtext in yellow colour"
