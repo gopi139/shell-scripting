ef8f30f76a7b6cfd52212c69e7b3577bdbccdd5d
saisuda9 <saikumarsuda@gmail.com>
2021-12-04 22:09:18 +0530

    creating 06-exit.sh file and add some content

---
+++ 06-exit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# exit status are between 0-255
+# 0 is success and other than 0 maybe failed / partially success
+# b/n 0-255 ,author use only 0-125 , from 126-255 are used by system default.
+
+exit 0
