4bd2bc2fb1409a093cc237a8f983064e8458e3a6
saisuda9 <saikumarsuda@gmail.com>
2021-12-04 22:51:00 +0530

    add new content

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# name assigned to set of commands is called function
+# function are used to keep the code  DRY(Don't Repeat Yourself) i.e reusability
 # function should be declared before using it
 
 function abd(){
