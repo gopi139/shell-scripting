92f9b3020acccdac9d8301a674bd9e227d57b740
saisuda9 <saikumarsuda@gmail.com>
2021-12-04 18:02:13 +0530

    add new lines

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -26,3 +26,22 @@ echo -e "your name:$1\nyour age:$2"
 # < = standard input
 # >> = double append .. it gives the previous data as well as current data
 
+# 1> used to output go to file(34-37 line) ; 2> used to error goto file(38-41 lines)
+# &> used to output and error are go to same file (43-46 lines)
+
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ ls -ld /boot /boo1
+# ls: cannot access /boo1: No such file or directory
+# dr-xr-xr-x. 5 root root 4096 Nov 11 17:00 /boot
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ ls -ld /boot /boo1 1>/tmp/out
+# ls: cannot access /boo1: No such file or directory
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ cat /tmp/out
+# dr-xr-xr-x. 5 root root 4096 Nov 11 17:00 /boot
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ ls -ld /boot /boo1 2>/tmp/err
+# dr-xr-xr-x. 5 root root 4096 Nov 11 17:00 /boot
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ cat /tmp/err
+# ls: cannot access /boo1: No such file or directory
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ ls -ld /boot /boo1 &>/tmp/total
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$ cat /tmp/total
+# ls: cannot access /boo1: No such file or directory
+# dr-xr-xr-x. 5 root root 4096 Nov 11 17:00 /boot
+# [ centos@ip-172-31-81-229 ~/shell-scripting ]$
