18e327f0d2dda00d1cf494a09e01e717ea80c8da
saisuda9 <saikumarsuda@gmail.com>
2021-12-05 23:49:59 +0530

    adding the content

--- 08-if.sh
+++ 08-if.sh
@@ -6,3 +6,9 @@ if [ "$username" == "root" ]; then
 else
     echo "you are a non root user"
 fi
+
+if [ $UID -eq 0 ]; then
+    echo "you are a root user"
+else
+    echo "you are non root user"
+fi
