6f6c0f691361696d01bc99dfc6c26bb7cd814fb2
saisuda9 <saikumarsuda@gmail.com>
2021-12-05 23:35:21 +0530

    modifying the content

--- 08 -if.sh
+++ 08 -if.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read -p 'enter your name:' = username
+read -p 'enter your name:' username
 
-if ["$username" == "root"]; then
+if [ "$username" == "root" ]; then
      echo " you are a root user"
 else
      echo "you are non root user"
