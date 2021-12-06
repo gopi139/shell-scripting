0a911e3967f1af48f88fa4e0e22c204a3d50b6fd
saisuda9 <saikumarsuda@gmail.com>
2021-12-06 17:54:53 +0530

    adding the content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,13 +5,6 @@ Print "installing nginx"
 yum install nginx -y &>>$LOG
 stat $?
 
-Print "enabling the nginx"
-systemctl enable nginx
-stat $?
-
-Print "starting the nginx"
-systemctl start nginx
-stat $?
 
 Print "download HTML pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
@@ -22,14 +15,22 @@ rm -rf  /usr/share/nginx/html/* &>>$LOG
 stat $?
 
 Print "extract frontend archive"
-unzip -d /tmp /tmp/frontend.zip &>>$LOG
+unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
 stat $?
 
 Print "copying files to nginx path"
-mv /tmp/frontend-main/static/*  /usr/share/nginx/html/ .
+mv /tmp/frontend-main/static/*  /usr/share/nginx/html/ .  &>>$LOG
 stat $?
 
-#mv static/* .
-#rm -rf frontend-master static README.md
-#mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#systemctl restart nginx
+Print "copy nginx roboshop config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+stat $?
+
+
+Print "enabling the nginx"
+systemctl enable nginx &>>$LOG
+stat $?
+
+Print "starting the nginx"
+systemctl restart nginx &>>$LOG
+stat $?
