3d2641a843ef31f42800b3155645a1002a837b05
saisuda9 <saikumarsuda@gmail.com>
2021-12-06 16:50:35 +0530

    adding the content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
+sourse componets/common.sh
 
-Print(){
-  echo -n -e "\e[1m$1\e[0m ......"
-# -n gives the continue to next line
-# ex:[ centos@ip-172-31-90-34 ~/shell-scripting/roboshop ]$ echo hello
-     #hello
-     #[ centos@ip-172-31-90-34 ~/shell-scripting/roboshop ]$ echo -n hello
-     #hello[ centos@ip-172-31-90-34 ~/shell-scripting/roboshop ]$
-
-  echo -e "\n\e[36m============ $1 =============\e[0m" >>$LOG
-}
-stat (){
-  if [ $1 -eq 0 ]; then
-      echo -e "\e[1;32msuccess\e[0m"
-  else
-      echo -e "\e[1;31mfailure\e[0m"
-      echo -e "\e[33mscript failed for detail log in $LOG file \e[0m"
-      exit 1
-  fi
-}
-LOG=/tmp/roboshop.log
-rm -f $LOG
 Print "installing nginx"
 yum install nginx -y &>>$LOG
 stat $?
