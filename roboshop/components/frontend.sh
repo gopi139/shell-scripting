885055e2a184e6e9e61ce9d3281021e8711d4dfa
saisuda9 <saikumarsuda@gmail.com>
2021-12-06 17:22:41 +0530

    adding the content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -13,15 +13,17 @@ Print "starting the nginx"
 systemctl start nginx
 stat $?
 
-exit
+Print "download HTML pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+stat $?
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+Print "remove old html pages"
+rm -rf  /usr/share/nginx/html/* &>>$LOG
+stat $?
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-master static README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+#unzip /tmp/frontend.zip
+#mv frontend-main/* .
+#mv static/* .
+#rm -rf frontend-master static README.md
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#systemctl restart nginx
