734e5fb9bf1c27da6a58d7d44495268522c70649
saisuda9 <saikumarsuda@gmail.com>
2021-12-06 22:37:04 +0530

    adding the content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -22,9 +22,9 @@ Print "copying files to nginx path"
 mv /tmp/frontend-main/static/*  /usr/share/nginx/html/.  &>>$LOG
 stat $?
 
-#Print "copy nginx roboshop config file"
-#cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
-#stat $?
+Print "copy nginx roboshop config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+stat $?
 
 
 #Print "enabling the nginx"
