5b8904ad7460d44c14ea4005ff4b6c3b8267a6c0
saisuda9 <saikumarsuda@gmail.com>
2021-12-08 20:19:31 +0530

    adding the content in redis .sh

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Print "install redis repos"
+yum install  yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+stat $?
+
+Print "enable redis repos"
+yum-config-manager --enable remi &>>$LOG
+stat $?
+
+Print "install redis"
+yum install redis -y &>>$LOG
+stat $?
+
+Print "update redis listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf  &>>$LOG
+stat $?
+
+Print "start redis"
+systemctl enable redis && systemctl start redis  &>>$LOG
+stat $?
