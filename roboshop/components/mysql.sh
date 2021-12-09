83a4961d9c23f317b79248b0fa9ac8f049d7a304
saisuda9 <saikumarsuda@gmail.com>
2021-12-09 23:09:32 +0530

    adding the content

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -22,8 +22,8 @@ NEW_PASSWORD=RoboShop@1
 echo 'show database;' | mysql -uroot -p"${NEW_PASSWORD}" &>>$LOG
 if [ $? -ne 0 ]; then
   Print "changing the default password"
-  echo -e "ALTER USER 'sudo root'@'localhost' IDENTIFIED BY '${NEW_PASSWORD}';\nuninstall plugin validate_password;" >/tmp/pass.sql
-  mysql -uroot -p"${NEW_PASSWORD}" </tmp/pass.sql &>>$LOG
+  echo -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${NEW_PASSWORD}';\nuninstall plugin validate_password;" >/tmp/pass.sql
+  mysql -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.sql &>>$LOG
 fi
 
 #
