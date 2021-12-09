d641a118e13c2fd2adbb8a22e7053549e08307ee
saisuda9 <saikumarsuda@gmail.com>
2021-12-09 14:08:35 +0530

    adding the content

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -62,7 +62,7 @@ NODEJS() {
   stat $?
 
   Print "update DNS record in systemd config"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/g' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/g' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/g' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
   stat $?
 
   Print "copy systemd file"
